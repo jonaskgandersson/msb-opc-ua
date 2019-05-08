9de24fe26c020e622011d2f75634aa66e4917854
Jonas Andersson <jonaskgandersson@gmail.com>
2019-05-08 11:49:57 +0200

    Fix compare of complete browse path and some refact

--- nodejs/parseTree.js
+++ nodejs/parseTree.js
@@ -1,9 +1,11 @@
+"use strict"
+
 var fs = require('fs')
 var path = require('path');
 var traverse = require('traverse');
 var mm = require("micromatch");
 
-var dataPath = path.join(__dirname, 'dataAll.json')
+var dataPath = path.join(__dirname, 'dataSmall.json')
 var data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 var configPath = path.join(__dirname, 'clientConfig.json')
@@ -13,21 +15,36 @@ var monitorList = {};
 
 traverse(data).forEach(function (x) {
 
+    var monitorItem = {};
+
     if (this.isLeaf && this.key === "browseName") {
-        
+
         var nodeBrowsePath = parentsToStringPath(this.parents);
 
-        findBrowseMatch( nodeBrowsePath, config.server.session.monitor.nodes );
+        var browseMatch = findBrowseMatch(nodeBrowsePath, config.server.session.monitor.nodes);
 
-        if (mm.any(nodeBrowsePath, config.server.session.monitor.crawl.any, { nocase: true }) &&
-            mm.all(nodeBrowsePath, config.server.session.monitor.crawl.all, { nocase: true }) &&
-            !mm.any(nodeBrowsePath, config.server.session.monitor.crawl.not, { nocase: true })
+        if (browseMatch) {
+            console.log("[Browse] Node: " + nodeBrowsePath + ", Id: " + this.parent.node.nodeId);
+
+            monitorItem["browseName"] = nodeBrowsePath;
+            monitorItem["node"] = this.parent.node;
+            monitorItem["settings"] = browseMatch[0].settings ? browseMatch[0].settings : config.server.session.monitor.settings;
+
+            monitorList[this.parent.node.nodeId] = monitorItem;
+
+        } else if (mm.any(nodeBrowsePath, config.server.session.monitor.crawler.any, { nocase: true }) &&
+            mm.all(nodeBrowsePath, config.server.session.monitor.crawler.all, { nocase: true }) &&
+            !mm.any(nodeBrowsePath, config.server.session.monitor.crawler.not, { nocase: true })
         ) {
-            console.log("Node: " + nodeBrowsePath + ", Id: " + this.parent.node.nodeId);
+            console.log("[Crawl] Node: " + nodeBrowsePath + ", Id: " + this.parent.node.nodeId);
 
-            monitorList[this.parent.node.nodeId] = this.parent.node;
+            monitorItem["browseName"] = nodeBrowsePath;
+            monitorItem["node"] = this.parent.node;
+            monitorItem["settings"] = config.server.session.monitor.settings;
+
+            monitorList[this.parent.node.nodeId] = monitorItem;
         }
-        
+
     }
 });
 
@@ -49,18 +66,17 @@ function parentsToStringPath(array) {
     return arrayPath.join('.');
 }
 
-function findBrowseMatch(browsePath, configData){
+function findBrowseMatch(browsePath, configData) {
 
-    traverse(configData).forEach(function(x){
-        if (this.isLeaf && this.key === "browsePath") {
-            var pattern = [];   // TO DO, change matching func to string
-            pattern.push(x)
-            
-            if (mm.any(browsePath, pattern, { nocase: true })
-            ) {
-                console.log("Node: " + browsePath + ", Id: " + this.parent.node.nodeId);                
-            }            
+    var match = traverse(configData).reduce(function (acc, x) {
+        if (this.isLeaf && this.key === "browsePath" && x) {
+
+            if (browsePath.toUpperCase() === x.toUpperCase()) {
+                acc.push(this.parent.node);
+            }
         }
-    })
+        return acc;
+    }, []);
 
+    return match.length ? match : null;
 }
